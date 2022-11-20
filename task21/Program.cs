f216c8673b25f3e8f81dd47452a666988c00a99e
Anya Denisova <bilberian@mail.ru>
2022-11-20 14:17:58 +0300

    tasks 21 and 23

---
+++ task21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Точка А. Введите координату Х точки А: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Точка А. Введите координату Y точки А: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Точка А. Введите координату Z точки А: ");
+int Az = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Точка B. Введите координату Х точки B: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Точка B. Введите координату Y точки B: ");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Точка B. Введите координату Z точки B: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+
+double distance = Math.Round(Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2) + Math.Pow(Az - Bz, 2)), 2);
+Console.WriteLine($"Расстояние между точками А и B равно: {distance}");
---
+++ task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+
+for (int i = 1; i <= number; i++)
+{
+    //Console.Write(Math.Pow(i, 3) + " ");
+    //без Math.Pow:
+    Console.Write(i * i * i + " ");
+};
+
+ //или с while:
+ Console.WriteLine();
+ int j = 1;
+ while (j <= number)
+ {
+    Console.Write(Math.Pow(j, 3) + " ");
+    j++;
+ }
